e654fd4c9a35da07486b78898960969db8f6ac87
Manuela Garcia <mgh9517@gmail.com>
2025-05-28 10:41:36 -0500

    Create a new test to learn different jest matchers

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,3 +1,21 @@
-export function toUpperCase (str: string) {
+export function toUpperCase (str: string): string {
   return str.toUpperCase()
+}
+
+export type stringInfo = {
+  lowerCase: string
+  upperCase: string
+  characters: string[]
+  length: number,
+  extraInfo: object | undefined
+}
+
+export function getStringInfo (str: string): stringInfo {
+  return {
+    lowerCase: str.toLowerCase(),
+    upperCase: str.toUpperCase(),
+    characters: Array.from(str),
+    length: str.length,
+    extraInfo: {}
+  }
 }
--- src/test/utils.spec.ts
+++ src/test/utils.spec.ts
@@ -1,14 +1,36 @@
-import { toUpperCase } from '../app/utils'
+import { getStringInfo, toUpperCase } from '../app/utils'
 
 describe('Utils test suite', () => {
   it('should return the uppercase of a valid string', () => {
     // Arrange
+    const sut = toUpperCase
     const expected = 'HELLO'
 
     // Act
-    const actual = toUpperCase('hello')
+    const actual = sut('hello')
 
     // Assert
     expect(actual).toBe(expected)
+  })
+
+  it('should return the string info of a valid string', () => {
+    const actual = getStringInfo('hello')
+
+    expect(actual.lowerCase).toBe('hello')
+    expect(actual.upperCase).toBe('HELLO')
+
+    expect(actual.characters.length).toBe(5)
+    expect(actual.characters).toEqual(['h', 'e', 'l', 'l', 'o'])
+    expect(actual.characters).toContain<string>('h')
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(['l', 'l', 'o', 'h', 'e'])
+    )
+
+    expect(actual.length).toBe(5)
+    expect(actual.extraInfo).toEqual({})
+    expect(actual.extraInfo).not.toBe(undefined)
+    expect(actual.extraInfo).not.toBeNull()
+    expect(actual.extraInfo).toBeDefined()
+    expect(actual.extraInfo).toBeInstanceOf(Object)
   })
 })
