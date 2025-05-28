886cddfc50cbf6fc70fd034bcee3986090c34efd
Manuela Garcia <mgh9517@gmail.com>
2025-05-28 10:48:01 -0500

    Good practice in writing meaningful tests

--- src/test/utils.spec.ts
+++ src/test/utils.spec.ts
@@ -13,24 +13,39 @@ describe('Utils test suite', () => {
     expect(actual).toBe(expected)
   })
 
-  it('should return the string info of a valid string', () => {
-    const actual = getStringInfo('hello')
+  describe('getStringInfo for arg "My-String"', () => {
+    test('return right length', () => {
+      const actual = getStringInfo('My-String')
+      expect(actual.characters).toHaveLength(9)
+    })
 
-    expect(actual.lowerCase).toBe('hello')
-    expect(actual.upperCase).toBe('HELLO')
+    test('return right lower case', () => {
+      const actual = getStringInfo('My-String')
+      expect(actual.lowerCase).toBe('my-string')
+    })
 
-    expect(actual.characters.length).toBe(5)
-    expect(actual.characters).toEqual(['h', 'e', 'l', 'l', 'o'])
-    expect(actual.characters).toContain<string>('h')
-    expect(actual.characters).toEqual(
-      expect.arrayContaining(['l', 'l', 'o', 'h', 'e'])
-    )
+    test('return right upper case', () => {
+      const actual = getStringInfo('My-String')
+      expect(actual.upperCase).toBe('MY-STRING')
+    })
 
-    expect(actual.length).toBe(5)
-    expect(actual.extraInfo).toEqual({})
-    expect(actual.extraInfo).not.toBe(undefined)
-    expect(actual.extraInfo).not.toBeNull()
-    expect(actual.extraInfo).toBeDefined()
-    expect(actual.extraInfo).toBeInstanceOf(Object)
+    test('return right characters', () => {
+      const actual = getStringInfo('My-String')
+      expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g'])
+      expect(actual.characters).toContain<string>('M')
+      expect(actual.characters).toEqual(
+        expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-'])
+      )
+    })
+
+    test('return defined extra info', () => {
+      const actual = getStringInfo('My-String')
+      expect(actual.extraInfo).toBeDefined()
+    })
+
+    test('return valid extra info', () => {
+      const actual = getStringInfo('My-String')
+      expect(actual.extraInfo).toEqual({})
+    })
   })
 })
