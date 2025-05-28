eee754f76f77dc93491eaf7980d9a0c4727085e0
Manuela Garcia <mgh9517@gmail.com>
2025-05-28 10:28:12 -0500

    How to properly structure a unit test

---
+++ README.md
@@ -0,0 +1,23 @@
+# Testing Course 1
+
+## Setup
+
+```bash
+npm install
+```
+
+## Run tests
+
+```bash
+npm test
+```
+
+## Structure of a properly written test
+
+AAA principles:
+
+- Arrange
+- Act
+- Assert
+
+Setup and teardown
--- src/test/utils.spec.ts
+++ src/test/utils.spec.ts
@@ -1,7 +1,14 @@
 import { toUpperCase } from '../app/utils'
 
 describe('Utils test suite', () => {
-  test('should return the uppercase of the string', () => {
-    expect(toUpperCase('hello')).toBe('HELLO')
+  it('should return the uppercase of a valid string', () => {
+    // Arrange
+    const expected = 'HELLO'
+
+    // Act
+    const actual = toUpperCase('hello')
+
+    // Assert
+    expect(actual).toBe(expected)
   })
 })
